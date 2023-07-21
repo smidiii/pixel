c326fc89074d3cc292ca2f96b6d452b4a5424b87
Elias Schmid <e.schmid@tum.de>
2023-07-21 13:22:34 +0200

    add tum-tampermonkey-script

---
+++ tampermonkey/tum.overlay.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         r/place TUM Template
+// @namespace    http://tampermonkey.net/
+// @version      6
+// @description  try to take over the canvas!
+// @author       placeDE Devs
+// @match        https://garlic-bread.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
+// @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
+// ==/UserScript==
+
+var overlayImage = null;
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+        const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+        overlayImage = document.createElement("img");
+        updateImage();
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;`;
+        canvasContainer.appendChild(overlayImage);
+    }, false);
+}
+
+function updateImage() {
+    overlayImage.src = "https://github.com/smidiii/pixel/blob/main/outputs_tum/overlay_target.png?raw=true&" + Date.now()
+}
+
+setInterval(function () {overlayImage.src = "https://github.com/smidiii/pixel/blob/main/outputs_tum/overlay_target.png?raw=true&" + Date.now()}, 30000);
