c69f3e038f0e5f96b6cfc23f1c85e5df30590365
Elias Schmid <e.schmid@tum.de>
2023-07-21 13:18:27 +0200

    generated output

--- bin/generate_tum.sh
+++ bin/generate_tum.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python pixel.py ./pictures ./target_config_tum.toml --config ";;./outputs_tum/overlay_target.png;./outputs_tum/overlay_priorities.png;./outputs_tum/overlay_combined.png;./outputs_tum/overlay_pixels.json;1;0;1;1" --config ";;./outputs_tum/default_target.png;./outputs_tum/default_priorities.png;./outputs_tum/default_combined.png;./outputs_tum/default_pixels.json;0;0;1;1" 
+python3 pixel.py ./pictures ./target_config_tum.toml --config ";;./outputs_tum/overlay_target.png;./outputs_tum/overlay_priorities.png;./outputs_tum/overlay_combined.png;./outputs_tum/overlay_pixels.json;1;0;1;1" --config ";;./outputs_tum/default_target.png;./outputs_tum/default_priorities.png;./outputs_tum/default_combined.png;./outputs_tum/default_pixels.json;0;0;1;1" 
