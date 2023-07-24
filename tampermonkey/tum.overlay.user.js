843ff2326eea8a55b410f46686fbe1c90ec5d516
Elias Schmid <e.schmid@tum.de>
2023-07-24 13:51:48 +0200

    added new images

--- tampermonkey/tum.overlay.user.js
+++ tampermonkey/tum.overlay.user.js
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
         const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
         overlayImage = document.createElement("img");
         updateImage();
-        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1500px;pointerEvents: 'none';`;
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2500px;height: 2000px;pointerEvents: 'none';`;
         canvasContainer.appendChild(overlayImage);
     }, false);
 }
